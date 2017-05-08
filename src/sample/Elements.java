c55dca59df61a17c13a05cbd50c63565c5caeb21
Gwandoya <nrdsthlm@gmail.com>
2017-05-08 20:21:09 +0200

    Fungerande MenuButtons i RO-tabben -minor update

--- src/sample/Elements.java
+++ src/sample/Elements.java
@@ -104,7 +104,7 @@ public class Elements {
             ro.setValue(8);
             setMenuItem(splitMenuButton, anchorPane, 8);
         });
-        splitMenuButton.setText("Value");
+        splitMenuButton.setText("Value: ");
         return splitMenuButton;
     }
 
@@ -117,6 +117,7 @@ public class Elements {
                 Text t1 = new Text(apStrings[0]);
                 TextFlow textFlow1 = new TextFlow(t1);
                 anchorPane.getChildren().add(textFlow1);
+                splitMenuButton.setText("Value: -8");
                 break;
             case -5:
                 splitMenuButton.getItems().set(1, splitMenuButton.getItems().get(1));
@@ -125,6 +126,7 @@ public class Elements {
                 Text t2 = new Text(apStrings[1]);
                 TextFlow textFlow2 = new TextFlow(t2);
                 anchorPane.getChildren().add(textFlow2);
+                splitMenuButton.setText("Value: -5");
                 break;
             case -3:
                 splitMenuButton.getItems().set(2, splitMenuButton.getItems().get(2));
@@ -133,6 +135,7 @@ public class Elements {
                 Text t3 = new Text(apStrings[2]);
                 TextFlow textFlow3 = new TextFlow(t3);
                 anchorPane.getChildren().add(textFlow3);
+                splitMenuButton.setText("Value: -3");
                 break;
             case -2:
                 splitMenuButton.getItems().set(3, splitMenuButton.getItems().get(3));
@@ -141,6 +144,7 @@ public class Elements {
                 Text t4 = new Text(apStrings[3]);
                 TextFlow textFlow4 = new TextFlow(t4);
                 anchorPane.getChildren().add(textFlow4);
+                splitMenuButton.setText("Value: -2");
                 break;
             case -1:
                 splitMenuButton.getItems().set(4, splitMenuButton.getItems().get(4));
@@ -149,6 +153,7 @@ public class Elements {
                 Text t5 = new Text(apStrings[4]);
                 TextFlow textFlow5 = new TextFlow(t5);
                 anchorPane.getChildren().add(textFlow5);
+                splitMenuButton.setText("Value: -1");
                 break;
             case 0:
                 splitMenuButton.getItems().set(5, splitMenuButton.getItems().get(5));
@@ -157,6 +162,7 @@ public class Elements {
                 Text t6 = new Text(apStrings[5]);
                 TextFlow textFlow6 = new TextFlow(t6);
                 anchorPane.getChildren().add(textFlow6);
+                splitMenuButton.setText("Value: 0");
                 break;
             case 1:
                 splitMenuButton.getItems().set(6, splitMenuButton.getItems().get(6));
@@ -165,6 +171,7 @@ public class Elements {
                 Text t7 = new Text(apStrings[6]);
                 TextFlow textFlow7 = new TextFlow(t7);
                 anchorPane.getChildren().add(textFlow7);
+                splitMenuButton.setText("Value: 1");
                 break;
             case 2:
                 splitMenuButton.getItems().set(7, splitMenuButton.getItems().get(7));
@@ -173,6 +180,7 @@ public class Elements {
                 Text t8 = new Text(apStrings[7]);
                 TextFlow textFlow8 = new TextFlow(t8);
                 anchorPane.getChildren().add(textFlow8);
+                splitMenuButton.setText("Value: 2");
                 break;
             case 3:
                 splitMenuButton.getItems().set(8, splitMenuButton.getItems().get(8));
@@ -181,6 +189,7 @@ public class Elements {
                 Text t9 = new Text(apStrings[8]);
                 TextFlow textFlow9 = new TextFlow(t9);
                 anchorPane.getChildren().add(textFlow9);
+                splitMenuButton.setText("Value: 3");
                 break;
             case 5:
                 splitMenuButton.getItems().set(9, splitMenuButton.getItems().get(9));
@@ -189,6 +198,7 @@ public class Elements {
                 Text t10 = new Text(apStrings[9]);
                 TextFlow textFlow10 = new TextFlow(t10);
                 anchorPane.getChildren().add(textFlow10);
+                splitMenuButton.setText("Value: 5");
                 break;
             case 8:
                 splitMenuButton.getItems().set(10, splitMenuButton.getItems().get(10));
@@ -197,6 +207,7 @@ public class Elements {
                 Text t11 = new Text(apStrings[10]);
                 TextFlow textFlow11 = new TextFlow(t11);
                 anchorPane.getChildren().add(textFlow11);
+                splitMenuButton.setText("Value: 8");
                 break;
         }
     }
